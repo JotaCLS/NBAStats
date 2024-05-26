ceb20f2ae3b3e0102e12bc983eaae94ad1a2c0fa
JoaoCLS <joao.carlos.santos@ua.pt>
2024-05-26 23:44:55 +0100

    listas e filtro

--- Frontend/Form1.cs
+++ Frontend/Form1.cs
@@ -21,6 +21,8 @@ namespace NBAproject
         {
             InitializeComponent();
             loadData();
+            InitializeComboBox1();
+            InitializeComboBox2();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -57,7 +59,7 @@ namespace NBAproject
 
             SqlCommand cmd = new SqlCommand("SELECT * FROM Team", cn);
             SqlDataReader reader = cmd.ExecuteReader();
-            listBox1.Items.Clear();
+            teams.Items.Clear();
 
             while (reader.Read())
             {
@@ -68,7 +70,7 @@ namespace NBAproject
                 T.Wins_losses = reader["wins_losses"].ToString();
                 T.DivisionId = reader["division_id"].ToString();
 
-                listBox1.Items.Add(T);
+                teams.Items.Add(T);
             }
 
             cn.Close();
@@ -81,23 +83,60 @@ namespace NBAproject
 
         public void ShowTeam()
         {
-            if (listBox1.Items.Count == 0 | currentTeam < 0)
+            if (teams.Items.Count == 0 | currentTeam < 0)
                 return;
             Team team = new Team();
-            team = (Team)listBox1.Items[currentTeam];
+            team = (Team)teams.Items[currentTeam];
             txtId.Text = team.TeamId;
             txtName.Text = team.TeamName;
             txtCity.Text = team.TeamCity;
             txtWL.Text = team.Wins_losses;
             txtDivisionId.Text = team.DivisionId;
 
+            LoadGames(team.TeamId);
+
         }
+
+        private void LoadGames(string teamId)
+        {
+            if (!verifySGBDConnection())
+                return;
+
+            SqlCommand cmd = new SqlCommand("GetGamesByTeam", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@TeamId", teamId);
+            SqlDataReader reader = cmd.ExecuteReader();
+            games_list.Items.Clear();
+
+            while (reader.Read())
+            {
+                Game game = new Game();
+                game.GameId = reader["GameId"].ToString();
+                game.Date = reader["gameDate"].ToString();
+                game.SeasonId = reader["season_Id"].ToString();
+
+
+                game.HomeTeamName = reader["HomeTeamName"].ToString();
+                game.HomeTeamPoints = reader["HomeTeamPoints"].ToString();
+                
+                
+                game.AwayTeamName = reader["AwayTeamName"].ToString();
+                game.AwayTeamPoints = reader["AwayTeamPoints"].ToString();
+                
+
+                games_list.Items.Add(game);
+            }
+
+            cn.Close();
+        }
+
+
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex >= 0)
+            if (teams.SelectedIndex >= 0)
             {
-                currentTeam = listBox1.SelectedIndex;
+                currentTeam = teams.SelectedIndex;
                 ShowTeam();
             }
         }
@@ -128,6 +167,183 @@ namespace NBAproject
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string team = teams.SelectedItem.ToString();
+        }
+
+        private void InitializeComboBox2()
+        {
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.Items.Add("");
+            comboBox2.Items.Add("Atlantic");
+            comboBox2.Items.Add("Central");
+            comboBox2.Items.Add("Southeast");
+            comboBox2.Items.Add("Northwest");
+            comboBox2.Items.Add("Pacific");
+            comboBox2.Items.Add("Southwest");
+
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(comboBox2_SelectedIndexChanged); 
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox2 = (ComboBox)sender;
+
+            string selectedDivision = (string)comboBox2.SelectedItem;
+
+            filtrarDivision(selectedDivision);
+        }
+
+        public void filtrarDivision(string selectedDivision)
+        {
+            if (!verifySGBDConnection())
+                return;
+
+            SqlCommand cmd = new SqlCommand("GetTeamsByDivision", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@DivisionName", selectedDivision);
+            SqlDataReader reader = cmd.ExecuteReader();
+            teams.Items.Clear();
+            games_list.Items.Clear();
+
+            while (reader.Read())
+            {
+                Team T = new Team();
+                T.TeamId = reader["id"].ToString();
+                T.TeamName = reader["teamName"].ToString();
+                T.TeamCity = reader["cityName"].ToString();
+                T.Wins_losses = reader["wins_losses"].ToString();
+                T.DivisionId = reader["division_id"].ToString();
+
+                teams.Items.Add(T);
+            }
+
+            cn.Close();
+
+            currentTeam = 0;
+            ShowTeam();
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtWL_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDivisionId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComboBox1()
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Items.Add("");
+            comboBox1.Items.Add("West");
+            comboBox1.Items.Add("East");
+
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(comboBox1_SelectedIndexChanged_1);
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            
+            string selectedConference = (string)comboBox1.SelectedItem;
+
+            filtrarConference(selectedConference);
+        }
+
+        private void filtrarConference(string conferenceName)
+        {
+            if (!verifySGBDConnection())
+                return;
+
+            SqlCommand cmd = new SqlCommand("GetTeamsByConference", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ConferenceName", conferenceName);
+            SqlDataReader reader = cmd.ExecuteReader();
+            teams.Items.Clear();
+            games_list.Items.Clear();
+
+            while (reader.Read())
+            {
+                Team T = new Team();
+                T.TeamId = reader["id"].ToString();
+                T.TeamName = reader["teamName"].ToString();
+                T.TeamCity = reader["cityName"].ToString();
+                T.Wins_losses = reader["wins_losses"].ToString();
+                T.DivisionId = reader["division_id"].ToString();
+
+                teams.Items.Add(T);
+            }
+
+            cn.Close();
+
+            currentTeam = 0;
+            ShowTeam();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Games_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void games_list_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
